f71b48ec80458cbffa56bf70aeef05c0b6becb20
Christian Meissl <meissl.christian@gmail.com>
2022-11-23 15:25:29 +0100

    drm: allow to claim a specific plane on an crtc

--- src/backend/drm/mod.rs
+++ src/backend/drm/mod.rs
@@ -77,7 +77,9 @@ pub mod node;
 pub(self) mod session;
 pub(self) mod surface;
 
-pub use device::{DevPath, DrmDevice, DrmEvent, EventMetadata as DrmEventMetadata, Time as DrmEventTime};
+pub use device::{
+    DevPath, DrmDevice, DrmEvent, EventMetadata as DrmEventMetadata, PlaneClaim, Time as DrmEventTime,
+};
 pub use error::Error as DrmError;
 pub use node::{CreateDrmNodeError, DrmNode, NodeType};
 #[cfg(feature = "backend_gbm")]
