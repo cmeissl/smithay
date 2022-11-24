31d77b73b22f383d092aa6c7b7de3941d6e2cbdf
Christian Meissl <meissl.christian@gmail.com>
2022-11-24 10:51:15 +0100

    utils: provide a overlaps or touches variant
    this changes Rectangle::overlaps to no longer
    return true for touching rectangles.
    the old behavior can be found in Rectangle::overlaps_or_touches

--- src/backend/renderer/multigpu/mod.rs
+++ src/backend/renderer/multigpu/mod.rs
@@ -371,8 +371,9 @@ impl<A: GraphicsApi> GpuManager<A> {
                                 Vec::<Rectangle<i32, BufferCoords>>::new(),
                                 |damage, mut rect| {
                                     // replace with drain_filter, when that becomes stable to reuse the original Vec's memory
-                                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) =
-                                        damage.into_iter().partition(|other| other.overlaps(rect));
+                                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) = damage
+                                        .into_iter()
+                                        .partition(|other| other.overlaps_or_touches(rect));
 
                                     for overlap in overlapping {
                                         rect = rect.merge(overlap);
@@ -503,8 +504,9 @@ impl<A: GraphicsApi> GpuManager<A> {
                                 })
                                 .fold(Vec::<Rectangle<i32, BufferCoords>>::new(), |damage, mut rect| {
                                     // replace with drain_filter, when that becomes stable to reuse the original Vec's memory
-                                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) =
-                                        damage.into_iter().partition(|other| other.overlaps(rect));
+                                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) = damage
+                                        .into_iter()
+                                        .partition(|other| other.overlaps_or_touches(rect));
 
                                     for overlap in overlapping {
                                         rect = rect.merge(overlap);
@@ -973,13 +975,16 @@ where
 
                 // cpu copy
                 damage.dedup();
-                damage.retain(|rect| rect.overlaps(Rectangle::from_loc_and_size((0, 0), buffer_size)));
+                damage.retain(|rect| {
+                    rect.overlaps_or_touches(Rectangle::from_loc_and_size((0, 0), buffer_size))
+                });
                 damage.retain(|rect| rect.size.h > 0 && rect.size.w > 0);
                 // merge overlapping rectangles
                 damage = damage.into_iter().fold(Vec::new(), |new_damage, mut rect| {
                     // replace with drain_filter, when that becomes stable to reuse the original Vec's memory
-                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) =
-                        new_damage.into_iter().partition(|other| other.overlaps(rect));
+                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) = new_damage
+                        .into_iter()
+                        .partition(|other| other.overlaps_or_touches(rect));
 
                     for overlap in overlapping {
                         rect = rect.merge(overlap);
@@ -1576,8 +1581,9 @@ where
                 .flat_map(|rect| rect.intersection(Rectangle::from_loc_and_size((0, 0), dmabuf.size())))
                 .fold(Vec::<Rectangle<i32, BufferCoords>>::new(), |damage, mut rect| {
                     // replace with drain_filter, when that becomes stable to reuse the original Vec's memory
-                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) =
-                        damage.into_iter().partition(|other| other.overlaps(rect));
+                    let (overlapping, mut new_damage): (Vec<_>, Vec<_>) = damage
+                        .into_iter()
+                        .partition(|other| other.overlaps_or_touches(rect));
 
                     for overlap in overlapping {
                         rect = rect.merge(overlap);
